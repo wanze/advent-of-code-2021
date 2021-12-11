7342426ca766c6c1cf0833641773cbc9f7a5011f
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2021-12-11 21:57:31 +0100

    Refactor day 11

--- src/11-dumbo-octopus/dumbo-octopus.ts
+++ src/11-dumbo-octopus/dumbo-octopus.ts
@@ -1,52 +1,11 @@
 export function flashesAfterSteps(octopuses: number[][], steps: number): number {
-  let nFlashes = 0;
+  let nTotalFlashes = 0;
 
   for (let step = 0; step < steps; step++) {
-    const flashPositions: Array<{x: number, y: number}> = [];
-    for (let y = 0; y < 10; y ++) {
-      for (let x = 0; x < 10; x++) {
-        const energy = octopuses[y][x];
-        if (energy < 9) {
-          octopuses[y][x] += 1;
-        } else if (energy === 9) {
-          flashPositions.push({ x, y });
-        }
-      }
-    }
-
-    flashPositions.forEach((pos) => {
-      flashRecursive(pos.x, pos.y);
-    });
+    nTotalFlashes += advanceStep(octopuses);
   }
 
-  return nFlashes;
-
-  function flashRecursive(x: number, y: number) {
-    if (x < 0 || y < 0 || x > 9 || y > 9) {
-      return;
-    }
-
-    const energy = octopuses[y][x];
-
-    if (energy === 0) {
-      return;
-    }
-
-    if (energy === 9) {
-      octopuses[y][x] = 0;
-      nFlashes++;
-      flashRecursive(x - 1, y - 1);
-      flashRecursive(x, y - 1);
-      flashRecursive(x + 1, y - 1);
-      flashRecursive(x - 1, y);
-      flashRecursive(x + 1, y);
-      flashRecursive(x - 1, y + 1);
-      flashRecursive(x, y + 1);
-      flashRecursive(x + 1, y + 1);
-    } else {
-      octopuses[y][x] += 1;
-    }
-  }
+  return nTotalFlashes;
 }
 
 export function stepAllOctopusesFlash(octopuses: number[][]): number {
@@ -54,56 +13,63 @@ export function stepAllOctopusesFlash(octopuses: number[][]): number {
   let nFlashes = 0;
 
   while (nFlashes !== 100) {
-    const flashPositions: Array<{x: number, y: number}> = [];
-    for (let y = 0; y < 10; y ++) {
-      for (let x = 0; x < 10; x++) {
-        const energy = octopuses[y][x];
-        if (energy < 9) {
-          octopuses[y][x] += 1;
-        } else if (energy === 9) {
-          flashPositions.push({ x, y });
-        }
-      }
-    }
-
-    nFlashes = 0;
-    flashPositions.forEach((pos) => {
-      nFlashes += flashRecursive(pos.x, pos.y);
-    });
-
+    nFlashes = advanceStep(octopuses);
     currentStep++;
   }
 
   return currentStep;
+}
 
-  function flashRecursive(x: number, y: number): number {
-    if (x < 0 || y < 0 || x > 9 || y > 9) {
-      return 0;
+function advanceStep(octopuses: number[][]): number {
+  const flashPositions: Array<{x: number, y: number}> = [];
+  let nFlashes = 0;
+
+  for (let y = 0; y < 10; y ++) {
+    for (let x = 0; x < 10; x++) {
+      const energy = octopuses[y][x];
+      if (energy < 9) {
+        octopuses[y][x] += 1;
+      } else if (energy === 9) {
+        flashPositions.push({ x, y });
+      }
     }
-
-    const energy = octopuses[y][x];
-
-    if (energy === 0) {
-      return 0;
-    }
-
-    let nFlashes = 0;
-
-    if (energy === 9) {
-      octopuses[y][x] = 0;
-      nFlashes++;
-      nFlashes += flashRecursive(x - 1, y - 1);
-      nFlashes += flashRecursive(x, y - 1);
-      nFlashes += flashRecursive(x + 1, y - 1);
-      nFlashes += flashRecursive(x - 1, y);
-      nFlashes += flashRecursive(x + 1, y);
-      nFlashes += flashRecursive(x - 1, y + 1);
-      nFlashes += flashRecursive(x, y + 1);
-      nFlashes += flashRecursive(x + 1, y + 1);
-    } else {
-      octopuses[y][x] += 1;
-    }
-
-    return nFlashes;
   }
+
+  flashPositions.forEach((pos) => {
+    nFlashes += flashRecursive(pos.x, pos.y, octopuses);
+  });
+
+  return nFlashes;
+}
+
+function flashRecursive(x: number, y: number, octopuses: number[][]): number {
+  if (x < 0 || y < 0 || x > 9 || y > 9) {
+    return 0;
+  }
+
+  const energy = octopuses[y][x];
+
+  if (energy === 0) {
+    return 0;
+  }
+
+  let nFlashes = 0;
+
+  if (energy === 9) {
+    octopuses[y][x] = 0;
+    nFlashes++;
+
+    nFlashes += flashRecursive(x - 1, y - 1, octopuses);
+    nFlashes += flashRecursive(x, y - 1, octopuses);
+    nFlashes += flashRecursive(x + 1, y - 1, octopuses);
+    nFlashes += flashRecursive(x - 1, y, octopuses);
+    nFlashes += flashRecursive(x + 1, y, octopuses);
+    nFlashes += flashRecursive(x - 1, y + 1, octopuses);
+    nFlashes += flashRecursive(x, y + 1, octopuses);
+    nFlashes += flashRecursive(x + 1, y + 1, octopuses);
+  } else {
+    octopuses[y][x] += 1;
+  }
+
+  return nFlashes;
 }
