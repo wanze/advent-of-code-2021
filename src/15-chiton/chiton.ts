0e0a3e4b8e2f42b1f3b354c7b08a4cf391c659cd
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2021-12-24 09:47:28 +0100

    Day 15

--- src/15-chiton/chiton.ts
+++ src/15-chiton/chiton.ts
@@ -1,6 +1,84 @@
+const { PriorityQueue } = require('dsa.js');
+
+type Node = {
+  x: number;
+  y: number;
+}
+
+export function lowestTotalRiskDijkstra(matrix: number[][]): number {
+  const maxX = matrix[0].length - 1;
+  const maxY = matrix.length - 1;
+  const queue = new PriorityQueue();
+  const visited: Node[] = [];
+
+  const startNode: Node = { x: 0, y: 0 };
+  queue.enqueue([0, startNode]);
+
+  while (queue.size > 0) {
+    const [risk, node]: [risk: number, node: Node] = queue.dequeue();
+    if (node.x === maxX && node.y === maxY) {
+      return risk;
+    }
+
+    if (visited.find(n => n.x === node.x && n.y === node.y)) {
+      continue;
+    }
+    visited.push(node);
+
+    neighbours(node).forEach((neighbour) => {
+      const { x, y } = neighbour;
+      if (x >= 0 && x <= maxX && y >= 0 && y <= maxY) {
+        queue.enqueue([risk + matrix[y][x], neighbour]);
+      }
+    });
+  }
+
+  return -1;
+
+  function neighbours(node: Node): Node[] {
+    const { x, y } = node;
+    return [{ x, y: y - 1}, { x: x + 1, y }, { x, y: y + 1 }, { x: x - 1, y}];
+  }
+}
+
+// Sorry 🙈
+export function expandMatrix(matrix: number[][]): number[][] {
+  const expandedMatrix: number[][] = new Array(matrix.length * 5);
+
+  let increasedVertical = matrix;
+  for (let i = 0; i < 5; i++) {
+    // expand vertical
+    for (let y = 0; y < matrix.length; y++) {
+      expandedMatrix[i * matrix.length + y] = [...increasedVertical[y]];
+    }
+    //expand horizontal
+    let increasedHorizontal = increaseMatrix(increasedVertical);
+    for (let j = 1; j < 5; j++) {
+      for (let y = 0; y < matrix.length; y++) {
+        const cols = expandedMatrix[i * matrix.length + y];
+        expandedMatrix[i * matrix.length + y] = [...cols, ...increasedHorizontal[y]];
+      }
+      increasedHorizontal = increaseMatrix(increasedHorizontal);
+    }
+    increasedVertical = increaseMatrix(increasedVertical);
+  }
+
+  return expandedMatrix;
+
+  function increaseMatrix(matrix: number[][]): number[][] {
+    return matrix.map((rows) => {
+      return rows.map((risk) => {
+        return risk === 9 ? 1 : risk + 1;
+      });
+    });
+  }
+}
+
+
 type Position = { x: number, y: number }
 
-export function lowestTotalRisk(map: number[][]): number {
+// This naive approach using recursion does not work on larger maps.
+export function lowestTotalRiskNaive(map: number[][]): number {
   const allPaths: Array<Position[]> = [];
   const maxY = map.length - 1;
   const maxX = map[0].length - 1;
